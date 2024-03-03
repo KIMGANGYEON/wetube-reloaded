9495b09d20d33a30902bf3d770b7fff408b8882b
KIMGANGYEON <rkddus3177@naver.com>
2024-03-03 19:22:55 +0900

    꾸준히

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,38 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => res.send("Watch");
-export const edit = (req, res) => res.send("Edit");
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
+export const trending = (req, res) => {
+  return res.render("home", { pageTitle: "Home", videos });
+};
+export const see = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watch ${video.title}` });
+};
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => res.send("Delete Video");
