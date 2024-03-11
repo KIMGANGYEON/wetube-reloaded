d420045f9e44f541b2e41ce51223aa5e29a13087
KIMGANGYEON <rkddus3177@naver.com>
2024-03-12 01:20:20 +0900

    홧팅

---
+++ src/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const handleListening = () =>
+  console.log(`Server listenting on port http://localhost:${PORT}🔥`);
+
+app.listen(PORT, handleListening);
