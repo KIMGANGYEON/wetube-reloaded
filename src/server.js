9f49e1437b48626ba47563d782e02f2b876eb236
KIMGANGYEON <rkddus3177@naver.com>
2024-03-14 00:10:56 +0900

    영차

--- src/server.js
+++ src/server.js
@@ -1,21 +1,21 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
+const handleHome = (req, res) => res.send("hello");
 
-const handleHome = (req, res) => {
-  return res.send("I love you");
-};
-
-app.get("/", logger, handleHome);
+app.use(logger);
+app.get("/", handleHome);
 
 const handleListening = () =>
   console.log(`Server listenting on port http://localhost:${PORT} 🔥`);
 
 app.listen(PORT, handleListening);
+
+// const handleHome = (req, res) => {
+//   return res.send("I love you");
+// };
