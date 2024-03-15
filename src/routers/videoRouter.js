fd8ad300ad00ea0a07fd6929e3cdc5a433f7de7a
KIMGANGYEON <rkddus3177@naver.com>
2024-03-15 22:46:25 +0900

    여기까징

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -9,6 +9,7 @@ import {
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", see);
+//숫자만 오게 하는 정규식
 videoRouter.get("/:id(\\d+)/edit", edit);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 videoRouter.get("/upload", upload);
